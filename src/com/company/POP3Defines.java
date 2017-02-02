5f91e5d6eb7c1a4244de3edd4e30c980eddac610
Terenfear <terenfear@gmail.com>
2017-02-02 15:10:11 +0300

    turned some of your c++ code into java code

--- src/com/company/POP3Defines.java
+++ src/com/company/POP3Defines.java
@@ -1,24 +1,30 @@
 package com.company;
 
+import java.io.File;
+
 /**
  * Created by ZloiY on 01-Feb-17.
  */
 public interface POP3Defines {
-    public static int POP3_PORT = 110;
-    public static int POP3_DEFAULT_NEGATIVE_RESPONSE = 0;
-    public static int POP3_DEFAULT_AFFERMATIVE_RESPONSE = 1;
-    public static int POP3_WELCOME_RESPONSE = 2;
-    public static int POP3_STATE_AUTHORIZATION = 1;
-    public static int POP3_STATE_TRANSACTION = 2;
-    public static int POP3_STATE_UPDATE = 4;
-    public static int POP3_STAT_RESPONSE = 16;
-    public static int POP3_MSG_STATUS_UNDEFINED = 0;
-    public static int POP3_MSG_STATUS_NEW = 1;
-    public static int POP3_MSG_STATUS_READ = 2;
-    public static int POP3_MSG_STATUS_REPLIED = 4;
-    public static int POP3_MSG_STATUS_DELETED = 8;
-    public static int POP3_MSG_STATUS_CUSTOM = 16;
-    public static String APP_TITLE = "pop3server";
-    public static String APP_VERSION = "1.0";
+    int POP3_PORT = 110;
+    int POP3_DEFAULT_NEGATIVE_RESPONSE = 0;
+    int POP3_DEFAULT_AFFIRMATIVE_RESPONSE = 1;
+    int POP3_WELCOME_RESPONSE = 2;
 
+    int POP3_STATE_AUTHORIZATION = 3;
+    int POP3_STATE_TRANSACTION = 4;
+    int POP3_STATE_UPDATE = 5;
+
+    int POP3_STAT_RESPONSE = 16;
+    int POP3_MSG_STATUS_UNDEFINED = 0;
+    int POP3_MSG_STATUS_NEW = 1;
+    int POP3_MSG_STATUS_READ = 2;
+    int POP3_MSG_STATUS_REPLIED = 4;
+    int POP3_MSG_STATUS_DELETED = 8;
+    int POP3_MSG_STATUS_CUSTOM = 16;
+    String APP_TITLE = "pop3server";
+    String APP_VERSION = "1.0";
+
+    String USERS_DOMAIN = File.pathSeparator + "Users";
+    String PASS_FILE = "pass.txt";
 }
